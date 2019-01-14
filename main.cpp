a1eed645a2f3335f471fcb82abde3eabcbcabfa8
Андрей Ястребов <43274275+KrauseBerg@users.noreply.github.com>
2019-01-14 17:57:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <math.h>
+
+
+
+//using namespace std;
+
+//int main(){
+//    unsigned long long int p, S=4;
+//    unsigned int k=1;
+//    BigInteger M;
+//    cout<<"Input simple number: ";
+//    cin>>p;
+//    M=pow(2,p)-1;
+//    while(k!=p-1){
+//        S=(((S*S)-2)%M);
+//        k++;
+//    }
+//    if(S==0)
+//        cout<<M<<": prostoe!";
+//    else cout<<M<<": sostavnoe!";
+//}
+
